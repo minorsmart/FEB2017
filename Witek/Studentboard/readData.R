54892a3dd3de93994b991e4724d6a8756452177f
witusj <wfth69@gmail.com>
2017-02-18 23:22:22 +0100

    Initiate

---
+++ Witek/Studentboard/readData.R
@@ -0,0 +1,36 @@
+library(gsheet)
+library(dplyr)
+library(radarchart)
+
+## Read in data
+url <- "https://docs.google.com/spreadsheets/d/1gq-DXkAf4QpZKcgQnABv634DKXjTCHepUi5rHw19Yeg/edit?usp=sharing"
+dfData <- gsheet2tbl(url)
+
+## Calculate means
+zCols <- select(dfData, matches("Z[0-9]"))
+uCols <- select(dfData, matches("U[0-9]"))
+sCols <- select(dfData, matches("S[0-9]"))
+vCols <- select(dfData, matches("V[0-9]"))
+
+cleanCols <- function(x) {
+  as.numeric(
+    gsub(pattern = "[A-Z][a-z]*\\s\\(|\\)", replacement = "", x, ignore.case = FALSE, perl = FALSE,
+                              fixed = FALSE, useBytes = FALSE)
+  )
+  }
+zCols <- data.frame(apply(zCols[1:7],2, cleanCols))
+uCols <- data.frame(apply(uCols[1:7],2, cleanCols))
+sCols <- data.frame(apply(sCols[1:7],2, cleanCols))
+vCols <- data.frame(apply(vCols[1:7],2, cleanCols))
+
+dfMeans <- data.frame(Z = rowMeans(zCols), U = rowMeans(uCols), S = rowMeans(sCols), V = rowMeans(vCols))
+row.names(dfMeans) <- dfData[[2]]
+
+
+
+rowIx <- 4
+
+scores <- t(dfMeans[rowIx,])
+scores <- data.frame(labs = rownames(scores), scores)
+
+chartJSRadar(scores = scores, maxScale = 5)
